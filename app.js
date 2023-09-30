72290e37c01a261a8648abed1807cb8db2af1e47
sreeram9061 <sreeramg043@gmail.com>
2023-09-30 18:44:08 -0400

    sse

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 const express =require("express")
 const productRouter=require("./routers/productRoters.js")
 const usersRouter=require("./routers/usersRouters.js")
+const cors = require("cors")
 
 const app = express()
+
+app.use(cors({
+    originf:"http://localhost:5173/",
+    methods:["get","post"]}))
+    
 app.use(express.json())
 
-app.use('/product',productRouter)
+app.use('/product', productRouter)
 app.use('/users',usersRouter)
+
 module.exports=app
